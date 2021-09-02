e9b265b9d2227acb92e9d2a8a68504a104e73918
Christian Kirschberg <ckirschberg@gmail.com>
2021-09-02 13:04:52 +0200

    initial

---
+++ screens/ChatScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+
+const ChatScreen = props => {
+ return (
+    <View>
+    
+    </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default ChatScreen;
